504e453d6b8e2a70b2e54512cec9bfa13af896c8
Ana Beatrice Schulz Staut <aschulz-@c2r3p1.42sp.org.br>
2026-05-18 10:08:18 -0300

    ex19

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,36 +6,73 @@
 /*   By: aschulz- <aschulz-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 15:51:40 by aschulz-          #+#    #+#             */
-/*   Updated: 2026/05/15 19:06:37 by aschulz-         ###   ########.fr       */
+/*   Updated: 2026/05/18 10:06:11 by aschulz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//comparar, caso  der negativo  siginifica que o segundo argumento é maior, ai precisamos trocar a posição para ordem assci
+#include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
 
 int ft_strcpm(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1 != '\0')
+	while (s1[i] == s2[i] && s1[i] != '\0')
 	{
 		i++;
 	}
 	return (s1[i] - s2[i]);
-} //se der negativo, significa que s2 > s1, precisa trocar de lugar pra ficar na ordem
+}
 
 void ft_swapa(char **s1, char **s2)
 {
 	char *temp;
-	int i;
-	
-	*temp = argv[]
+
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
 	
 }
 
-
 int main (int argc, char **argv)
 {
-	
+	int i;
+	int j;
+
+	i = 1;
+	j = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcpm(argv[i], argv[i+1]) > 0)
+		{
+			ft_swapa(&argv[i], &argv[i+1]);
+			i = 1;
+		}
+		else
+			i++;
+	}
+	while (j < argc)
+	{
+		ft_putstr(argv[j]);
+		ft_putchar('\n');
+		j++;
+	}
+	return (0);
 }
